b27a4bc83e4a402bf14ac5c70bbf8f06ef876007
Yurii Kabannik <yurii.kabannik@gmail.com>
2025-02-12 02:05:22 +0200

    fix: Add ERC20 approval logic

--- interface/web3/hooks/use-create-project-mutation.ts
+++ interface/web3/hooks/use-create-project-mutation.ts
@@ -4,6 +4,7 @@ import {Address, parseEther} from "viem";
 import {config} from "@/web3/config";
 import {PROJECTS_REGISTRY_ABI} from "@/web3/abi/projects-registry-abi";
 import {PROJECTS_REGISTRY_ADDRESS} from "@/web3/constants";
+import {ERC20_ABI} from "@/web3/abi/erc20-abi";
 
 type UseCreateProjectMutationParameters = {
   name: string;
@@ -19,6 +20,16 @@ type UseCreateProjectMutationParameters = {
 export function useCreateProjectMutation() {
   return useMutation({
     mutationFn: async (params: UseCreateProjectMutationParameters) => {
+      const reward = parseEther(params.reward.toString());
+      const { request: approveRequest } = await simulateContract(config, {
+        address: params.token,
+        abi: ERC20_ABI,
+        functionName: "approve",
+        args: [PROJECTS_REGISTRY_ADDRESS, reward * BigInt(params.maxAssignments)],
+      });
+      const approveHash = await writeContract(config, approveRequest);
+      await waitForTransactionReceipt(config, { hash: approveHash });
+
       const expiresAt = Math.floor(params.expiresAt.getTime() / 1000);
       const { request } = await simulateContract(config, {
         address: PROJECTS_REGISTRY_ADDRESS,
@@ -32,7 +43,7 @@ export function useCreateProjectMutation() {
           params.teachers,
           BigInt(params.maxAssignments),
           params.token,
-          parseEther(params.reward.toString()),
+          reward,
         ],
       });
       const hash = await writeContract(config, request);
